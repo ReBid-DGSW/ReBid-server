a6b14381f3acae50b7e05a32593858ede17d5525
박규민 <131234408+kyumin7487@users.noreply.github.com>
2025-04-10 09:52:41 +0900

    Add :: CustomOAuth2User

---
+++ src/main/java/com/retrade/security/oauth2/CustomOAuth2User.java
@@ -0,0 +1,35 @@
+package com.retrade.security.oauth2;
+
+import com.retrade.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@AllArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+    private User user;
+    private Map<String, Object> attributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getName() {
+        return user.getEmail();
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
