7edf708954b750e6e8e787392844f728a6266315
박규민 <131234408+kyumin7487@users.noreply.github.com>
2025-05-26 08:49:27 +0900

    fix :: add @EnableAsync @EnableScheduling

--- src/main/java/com/retrade/RetradeBackendApplication.java
+++ src/main/java/com/retrade/RetradeBackendApplication.java
@@ -2,12 +2,15 @@ package com.retrade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class RetradeBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RetradeBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RetradeBackendApplication.class, args);
+    }
 }
