a09ea70486e8fe6ffcf7ee8b52ae3e8bcbc0c49d
박규민 <131234408+kyumin7487@users.noreply.github.com>
2025-05-28 08:42:24 +0900

    tada :: 셋팅

---
+++ src/main/java/com/retrade/RetradeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.retrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetradeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetradeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retrade/RetradeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetradeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
