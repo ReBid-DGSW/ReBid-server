34d916575f819cde46cc652300d7a83decac6a4c
박규민 <131234408+kyumin7487@users.noreply.github.com>
2025-06-23 10:24:27 +0900

    Add :: SchedulerConfig

---
+++ src/main/java/com/retrade/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.retrade.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
